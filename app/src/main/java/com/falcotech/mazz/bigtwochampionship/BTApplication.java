677e0d72abfdefcc79714d566bdeb4ca10745cee
pcmazz <pcmaslow@alumni.ucsd.edu>
2017-04-21 21:28:03 -0500

    Gradle Issue

--- app/src/main/java/com/falcotech/mazz/bigtwochampionship/BTApplication.java
+++ app/src/main/java/com/falcotech/mazz/bigtwochampionship/BTApplication.java
@@ -2,6 +2,8 @@ package com.falcotech.mazz.bigtwochampionship;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * Created by phima on 4/21/2017.
  */
@@ -9,7 +11,20 @@ import android.app.Application;
 public class BTApplication extends Application {
     private BTApplicationComponent applicationComponent;
 
+    public BTApplicationComponent getApplicationComponent(){
+        return this.applicationComponent;
+    }
+
     private void initialize(){
 
     }
+
+    private void installLeakCanary(){
+        if(LeakCanary.isInAnalyzerProcess(this)){
+            return;
+        }
+        LeakCanary.install(this);
+    }
+
+
 }
